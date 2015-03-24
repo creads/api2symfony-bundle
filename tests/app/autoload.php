67370efcae5f72f1e3e36b534792492d0e77ba1f
qpautrat <quentin.pautrat@gmail.com>
2015-03-24 14:26:32 +0100

    fix bootstraping files

--- tests/app/autoload.php
+++ tests/app/autoload.php
@@ -1,5 +1,13 @@
 <?php
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
+/**
+ * @var ClassLoader $loader
+ */
 $loader = require __DIR__.'/../../vendor/autoload.php';
 
-require __DIR__.'/AppKernel.php';
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+return $loader;
